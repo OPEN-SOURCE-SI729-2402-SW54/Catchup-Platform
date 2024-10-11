e4ea169f9cb67c7db5c73a186706c35001b7fbfe
Diego Ulises Soto Quispe <148501686+Diego2005z@users.noreply.github.com>
2024-10-10 20:21:50 -0500

    Subiendo proyecto inicial Catchup-Platform

---
+++ src/main/java/pe/upc/catchupplataform/CatchUpPlataformApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.catchupplataform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchUpPlataformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchUpPlataformApplication.class, args);
+    }
+
+}
